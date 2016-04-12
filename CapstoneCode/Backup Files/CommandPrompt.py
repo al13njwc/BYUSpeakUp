4f8d5990f529ca4128d1550f582347d61d88f9e9
SpeakUpHub <tayjmorgan@gmail.com>
2016-04-11 23:26:40 -0600

    Adding first set of new files

--- CapstoneCode/Backup Files/CommandPrompt.py
+++
@@ -1,8 +0,0 @@
-import subprocess
-import time
-
-
-proc = subprocess.Popen('cmd.exe', stdin = subprocess.PIPE, stdout = subprocess.PIPE)
-stdout, stderr = proc.communicate('dir c:\\')
-stdout 'ping www.google.com'
-time.sleep(10) #Delays the player to stop after X seconds
--- README.md
+++
@@ -1,2 +0,0 @@
-# BYUSpeakUp
-BYU capstone project
